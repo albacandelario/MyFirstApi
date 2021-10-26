ecd7489e655c59e31ba98bfce7080d0f526f6d6d
albacandelario <alba.candelario@gracia.lasalle.cat>
2021-10-26 19:54:02 +0200

    26 oct

--- controllers/s04-05Controller.js
+++ controllers/s04-05Controller.js
@@ -1,22 +1,64 @@
 const status = {
     ok: 200,
     notFound: 404,
+    badRquest: 400,
 };
 
 module.exports = {
     fechaMs: (req, res) => {
+        // let fecha = `${new Date().getTime()} ms`;
         let fecha = new Date().getTime();
-        res.status(status.ok).send({dataActual: fecha});
+        res.status(status.ok).send({ dataActual: fecha });
     },
 
     fechaGuiones: (req, res) => {
-
+        let fecha = `${new Date().getDate()}-${new Date().getMonth() + 1}-${new Date().getFullYear()}`;
+        res.status(status.ok).send({ dataActual: fecha });
     },
 
     fechaPuntos: (req, res) => {
-
+        let fecha = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`;
+        res.status(status.ok).send({ dataActual: fecha });
     },
 
-    
+    multiplicar: (req, res) => {
+        const input = req.params.input;
+        const tabla = [];
 
+        if (input >= 2 && input <= 5) {
+            for (let i = 0; i <= 10; i++) {
+                tabla.push(`${i} x ${input} = ${input * i}`)
+            }
+            res.status(status.ok).send({ Tabla: tabla });
+        } else {
+            res.status(status.badRquest).send("Debes introducir un número del 2 al 5!!");
+        }
+    },
+
+    factorial: (req, res) => {
+        const input = req.query.input;
+
+        if (isNaN(input)) {
+            res.status(status.badRquest).send("No has escrito un número!");
+        } else if ((input % 1) === 0) {
+            let result = `${input}`;
+            let resultNum = input;
+            for (let i = (input - 1); i > 0; i--) {
+                result += ` * ${i}`;
+                resultNum *= i;
+            }
+            result += ` = ${resultNum}`;
+            res.status(status.ok).send({ Factorial: result });
+        } else {
+            res.status(status.badRquest).send("No has escrito un número entero!");
+        }
+    },
+
+    operacion: (req, res) => {
+        const operation = req.body.operation;
+        const num1 = req.body.num1;
+        const num2 = req.body.num2;
+
+
+    },
 }
--- routes/s04-05Routes.js
+++ routes/s04-05Routes.js
@@ -6,6 +6,9 @@ const ejController = require('../controllers/s04-05Controller.js');
 router.get('/ej01FechaMs', ejController.fechaMs);
 router.get('/ej01FechaGuiones', ejController.fechaGuiones);
 router.get('/ej01FechaPuntos', ejController.fechaPuntos);
+router.get('/ej02Multiplicar/:input', ejController.multiplicar);
+router.get('/ej03Factorial', ejController.factorial);
+router.get('/ej04Operacion', ejController.operacion);
 
 
 module.exports = router;
